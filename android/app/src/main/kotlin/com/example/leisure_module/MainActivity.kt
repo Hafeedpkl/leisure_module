5dc44b942ebe0bd6716506416cd260da90d44957
hafeed <hafeedpallikkal@gmail.com>
2023-05-09 19:45:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/leisure_module/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.leisure_module
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/grid_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GridModel {
+  String image;
+  String text;
+  Color color;
+  GridModel({required this.image, required this.text, required this.color});
+}
